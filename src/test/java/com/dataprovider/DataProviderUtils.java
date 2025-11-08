974bbf095ecf82bd1141aa341fd82cf8bfa82167
Rupesh <rupesh0101@hotmail.com>
2025-11-08 23:11:51 +0530

    added excel data driven test for create job api

--- src/test/java/com/dataprovider/DataProviderUtils.java
+++ src/test/java/com/dataprovider/DataProviderUtils.java
@@ -7,10 +7,12 @@ import java.util.List;
 import org.testng.annotations.DataProvider;
 
 import com.api.request.model.CreateJobPayload;
+import com.api.request.model.Login;
 import com.api.request.model.LoginUserCredentials;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
 import com.api.utils.ExcelReaderUtil;
+import com.api.utils.ExcelReaderWithPoiji;
 import com.api.utils.FakerDataGenerator;
 import com.api.utils.JsonReaderUtil;
 import com.dataprovider.api.bean.CreateJobBean;
@@ -64,6 +66,36 @@ public class DataProviderUtils {
 	@DataProvider(name = "loginAPiExcelDataProvider", parallel = true)
 	public static Iterator<LoginUserCredentials> loginAPiExcelDataProvider() {
 		return ExcelReaderUtil.loadExcelTestData("testData/LoginApiTestData.xlsx");
+
+	}
+
+	@DataProvider(name = "loginAPiExcelPoijiDataProvider", parallel = true)
+	public static Iterator<LoginUserCredentials> loginAPiExcelPoijiDataProvider() {
+		return ExcelReaderWithPoiji.loadExcelTestDataFromPoiji("testData/CreateJobData.xlsx",
+				LoginUserCredentials.class);
+
+	}
+
+	@DataProvider(name = "createJobExcelPoijiDataProvider", parallel = true)
+	public static Iterator<CreateJobBean> createJobExcelPoijiDataProvider() {
+		return ExcelReaderWithPoiji.loadExcelTestDataFromPoiji("testData/createJobData.xlsx", CreateJobBean.class);
+
+	}
+
+	@DataProvider(name = "createJobApiExcelDataProvider", parallel = true)
+	public static Iterator<CreateJobPayload> createJobApiExcelDataProvider() {
+		Iterator<CreateJobBean> iterator = ExcelReaderWithPoiji
+				.loadExcelTestDataFromPoiji("testData/createJobData1.xlsx", CreateJobBean.class);
+		List<CreateJobPayload> payloadList = new ArrayList<CreateJobPayload>();
+		CreateJobBean tempBean;
+		CreateJobPayload tempPayload;
+		while (iterator.hasNext()) {
+			tempBean = iterator.next();
+			tempPayload = CreateJobBeanMapper.mapper(tempBean);
+			payloadList.add(tempPayload);
+		}
+		
+		return payloadList.iterator();
 
 	}
 
